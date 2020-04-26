e5eb9c7637ff8c27f2d61813d81b41ae38fc5184
mihir97 <mistrymu15.comp@coep.ac.in>
2020-04-26 23:02:43 +0530

    chatbot changes

--- chatbot.py
+++ chatbot.py
@@ -7,6 +7,23 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 INTENT_DOCTOR = 'Register_Agent'
 INTENT_COMMUNITY = 'Register_Community'
 
+model_name = "en_core_web_sm"
+
+import spacy
+import csv
+from spacy.cli.download import download
+download(model_name)
+#from spacy.cli import link
+#from spacy.util import get_package_path
+#package_path = get_package_path(model_name)
+#link(model_name, model_name, force=True, package_path=package_path)
+
+nlp = spacy.load('en_core_web_sm')
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+replytext ='empty msg'
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -20,10 +37,9 @@ service = ibm_watson.AssistantV2(
 service.set_service_url(config["ibm_assistant"]["url"])
 
 def handle_message(from_no, message):
-    response =  get_response(from_no, message)
+    reply =  get_response(from_no, message)
     #print(json.dumps(response, indent=2))
 
-    reply = response["output"]["generic"][0]["text"]
     #Split message on <#>
     replies = reply.split('<#>')
     return replies
@@ -31,20 +47,28 @@ def handle_message(from_no, message):
 
 
 def get_response(from_no, message):
-    session = db.getSessionID(from_no)
-    response = service.message(
-        assistant_id=config["ibm_assistant"]["assistant_id"],
-        session_id=session,
-        input={
-            'message_type': 'text',
-            'text': message
-        }
-    ).get_result()
-    #print(json.dumps(response, indent=2))
-    #res = json.dumps(response)
-    #reply = response["output"]["generic"][0]["text"]
-    #print(reply)
-    return response
+    gotreply = False
+    global replytext
+    with open('FAQ.csv', encoding="utf8") as csv_file:
+        csv_reader=csv.reader(csv_file , delimiter=',')
+
+        for row in csv_reader:
+            similarity = nlp(row[0].lower()).similarity(nlp(message.lower()))
+            #print (similarity)
+            #print (str(message), str(row[0]))
+            if similarity > 0.70:
+                gotreply = True
+                replytext = str(row[1])
+
+    if gotreply==False:
+        replytext = "Jake was not able to answer your query. Our agents will reach you soon."
+
+    print("what caller said--------", message)
+    print("reply text is ------", replytext)
+    
+    msgBody = "Hi! This is Jake from State farm. Answer to your query - " + replytext
+
+    return msgBody
 
 def new_session():
     response = service.create_session(
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 import json
 import twilio_messaging as messaging
@@ -9,6 +9,16 @@ import os
 import statefarm_handler as statefarm
 from time import sleep
 from threading import Thread
+
+
+from twilio.twiml.voice_response import VoiceResponse
+from twilio import twiml
+import csv
+import requests
+import time
+from twilio.rest import Client
+from twilio.twiml.voice_response import Record, VoiceResponse, Gather
+from xml.etree import ElementTree
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -91,6 +101,118 @@ def sendMessage():
     message = request.values.get('message', None)
     expert = messaging.send_message(userID, message)
     return str("Success")
+
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    resp = VoiceResponse()
+
+    g = Gather(num_digits=1, action='/handleVoice')
+    g.say("Welcome, press one for English", voice='alice', language="en-US")
+    g.say("Bienvenido, para español, presione 2", voice='alice', language="es-MX")
+    g.say("欢迎，按3为中文", voice='alice', language="zh-CN")
+
+    resp.append(g)
+
+    resp.redirect('/voice')
+
+    return str(resp)
+
+
+@app.route("/handleVoice", methods=['GET', 'POST'])
+def handleVoice():
+    resp2 = VoiceResponse()
+
+    if 'Digits' in request.values:
+        # Get which digit the caller chose
+        choice = request.values['Digits']
+        phone = request.values['From']
+
+        if choice == '1':
+            gg =Gather(num_digits=1, action='/recordAndSend')
+            gg.say("Hi, all our agents are busy right now. If your query is not urgent, would you like to leave a msg for us? We will get back to you with the response very soon. Press 1 to leave a msg. Press 2 to wait for the agent.", voice='alice', language="en-US")
+
+            resp2.append(gg)
+
+            print(resp2)
+            return str(resp2)
+
+        elif choice == '2':
+            gg =Gather(num_digits=1, action='/recordAndSend')
+            gg.say("Hola, todos nuestros agentes están ocupados en este momento. Si su consulta no es urgente, ¿desea dejarnos un mensaje? Nos pondremos en contacto con usted con la respuesta muy pronto. Presione 1 para dejar un mensaje. Presione 2 para esperar al agente.", voice='alice', language="es-MX")
+
+            resp2.append(gg)
+
+            print(resp2)
+            return str(resp2)
+
+        elif choice == '3':
+            gg =Gather(num_digits=1, action='/recordAndSend')
+            gg.say("嗨，我们所有的经纪人现在都在忙。如果您的查询不紧急，您想给我们留言吗？我们会尽快回复您。按1离开留言。按2等待代理。", voice='alice', language="zh-CN")
+
+            resp2.append(gg)
+
+            print(resp2)
+            return str(resp2)
+
+        else:
+            resp2.say("Sorry, I dint understand the input")
+            return ""
+
+    return str(resp2)
+
+@app.route("/handleVoiceResponse", methods=['GET', 'POST'])
+def handleVoiceResponse():
+    #resp = VoiceResponse()
+    soundURL = (request.values['RecordingUrl'])
+    print (request.values)
+    calltext = request.values['TranscriptionText']
+    caller = request.values['From']
+
+    msgBody = chatbot.handle_message(caller, calltext)
+    messaging.send_messages(caller, msgBody)
+
+    return 'success'
+
+
+@app.route("/recordAndSend", methods=['GET','POST'])
+def recordAndSend():
+    resp = VoiceResponse()
+
+    if 'Digits' in request.values:
+        choice = request.values['Digits']
+
+        if choice == '1':
+            resp.say("Hi, how can I help you today?", voice='alice', language="en-US")
+            resp.record(maxLength = 20,timeout=10, transcribe=True, transcribeCallback="/handleVoiceResponse")
+
+            print (resp)
+            return str(resp)
+
+        if choice == '2':
+            resp.say("Please stay online.", voice='alice', language="en-US")
+            resp.hangup()
+
+    else:
+        resp.say("We got your msg. Thank you.", voice='alice', language="en-US")
+        resp.hangup()
+
+    return str(resp)
+
+'''
+@app.route("/saythis", methods=['GET', 'POST'])
+def saythis():
+    replytext2 = request.args.get('reptext')
+    resp = VoiceResponse()
+    resp.say(replytext2, voice='alice', language="en-US")
+    return 'ok'
+'''
+
+def twiml(resp):
+    resp = flask.Response(str(resp))
+    resp.headers['Content-Type'] = 'text/xml'
+    return resp
+
 
 if __name__ == "__main__":
 
